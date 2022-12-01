af4cef23f1cc5ef21cd14c728319686a17c39c20
Brandon <brandon.michael.burt@gmail.com>
2022-12-01 00:17:42 -0500

    day 1

--- src/puzzles/day-01.ts
+++ src/puzzles/day-01.ts
@@ -2,6 +2,18 @@
 
 export function solvePuzzle1(input: string): [number, number] {
 
-    return [0, 0];
+    let data: string[] = input.split('\n');
+    let curr = 0, calories = [];
+    data.forEach(x => {
+        if (x === '') {
+            calories.push(curr);
+            curr = 0;
+        } else {
+            curr += +x;
+        }
+    });
+    calories.sort((a, b) => b-a);
+
+    return [calories[0], calories[0] + calories[1] + calories[2]];
 
 }
