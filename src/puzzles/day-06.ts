b8d4125b4edefc0e1345206f35773a2d3b21dad9
Brandon <brandon.michael.burt@gmail.com>
2022-12-06 00:25:07 -0500

    day 6

--- src/puzzles/day-06.ts
+++ src/puzzles/day-06.ts
@@ -2,6 +2,11 @@
 
 export function solvePuzzle6(input: string): [number, number] {
 
-    return [0, 0];
+    const findIndex = (len: number): number => {
+        for (let i=len; i<input.length; i++) {
+            if (new Set(input.substring(i-len, i)).size === len) return i;
+        }
+    }
+    return [findIndex(4), findIndex(14)];
 
 }
